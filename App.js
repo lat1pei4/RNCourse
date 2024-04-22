b92f6f8f384034a3cdd9a42655d918b5459f3594
Karl Kwok <23aw0111@jec.ac.jp>
2024-04-22 10:41:32 +0900

    interface and core function updated

--- App.js
+++ App.js
@@ -1,30 +1,58 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  function goalInputHandler(event) {
+    setEnteredGoal(event);
+  }
+
+  function addGoalHandler() {
+    setGoals((currentGoals) => [...currentGoals, enteredGoal]);
+    setEnteredGoal("");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>haha</Text>
-      <Button
-        title="click me"
-        onPress={() => {
-          console.log("button clicked");
-        }}
-      />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your goal"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: "red",
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomColor: "#cccccc",
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    width: "70%",
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
